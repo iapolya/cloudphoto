57b96930d17948ba9e8ec038b836a3f4b63fb0ed
Polina Alikina <iapolly@stride.one>
2020-12-29 01:02:53 +0300

    + ошибка при отсутвии каталога при скачивании файла

--- src/actions/download.ts
+++ src/actions/download.ts
@@ -6,10 +6,16 @@ import * as fs from "fs";
 import {download} from "../aws3";
 
 export const downloadFiles = (params: UploadParameters) => {
-    let albumParts = params.album.split('/');
-    listFiles().then((files) => files.forEach((content: _Object) => {
-        writeFile(content, albumParts, params);
-    }));
+    try {
+        fs.statSync(params.path);
+        let albumParts = params.album.split('/');
+        listFiles().then((files) => files.forEach((content: _Object) => {
+            writeFile(content, albumParts, params);
+        }));
+    } catch (e) {
+        console.log('Каталога не существует');
+    }
+
 };
 
 function writeFile(content, albumParts, params) {
